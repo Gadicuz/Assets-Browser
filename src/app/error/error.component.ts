0cc367479aa0aae976d5938e14c69ce07aa09bbf
Nikolay Postnikov <nick.postnikov@gmail.com>
2019-08-23 22:24:14 +0300

    Fix ts error message

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
 
-  @Input() err: Error;
+  @Input() err: any;
 
   constructor() { }
 
