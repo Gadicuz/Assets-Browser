833cd9b8a565ee4ed52d83869641b0988b279721
Nikolay Postnikov <nick.postnikov@gmail.com>
2020-01-15 18:22:47 +0300

    Implement x-www-form-urlencoded codec as separate file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,42 +2,11 @@ import { Component } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import { EVESSOService } from './services/EVESSO.service';
 
-import { HttpUrlEncodingCodec, HttpParameterCodec } from '@angular/common/http';
+import { X_WWW_FORM_UrlEncodingCodec } from './x-www-form-codec';
 
 import ru from '@angular/common/locales/ru';
 
 import ccpCopyright from '../assets/ccp.copyright.json';
-
-class X_WWW_FORM_UrlEncodingCodec implements HttpParameterCodec {
-
-  static hook() {
-    HttpUrlEncodingCodec.prototype.decodeKey = X_WWW_FORM_UrlEncodingCodec.prototype.decodeKey;
-    HttpUrlEncodingCodec.prototype.encodeKey = X_WWW_FORM_UrlEncodingCodec.prototype.encodeKey;
-    HttpUrlEncodingCodec.prototype.decodeValue = X_WWW_FORM_UrlEncodingCodec.prototype.decodeValue;
-    HttpUrlEncodingCodec.prototype.encodeValue = X_WWW_FORM_UrlEncodingCodec.prototype.encodeValue;
-  }
-
-  private static encode(v: string): string {
-    return encodeURIComponent(v).replace(/%20/gi, '+');
-  }
-
-  private static decode(v: string): string {
-    return decodeURIComponent(v.replace(/\\+/gi, '%20'));
-  }
-
-  encodeKey(key: string): string {
-    return X_WWW_FORM_UrlEncodingCodec.encode(key);
-  }
-  encodeValue(value: string): string {
-    return X_WWW_FORM_UrlEncodingCodec.encode(value);
-  }
-  decodeKey(key: string): string {
-    return X_WWW_FORM_UrlEncodingCodec.decode(key);
-  }
-  decodeValue(value: string): string {
-    return X_WWW_FORM_UrlEncodingCodec.decode(value);
-  }
-}
 
 @Component({
   selector: 'app-root',
---
+++ src/app/x-www-form-codec.ts
@@ -0,0 +1,33 @@
+import { HttpUrlEncodingCodec, HttpParameterCodec } from '@angular/common/http';
+
+export class X_WWW_FORM_UrlEncodingCodec implements HttpParameterCodec {
+
+  static hook() {
+    HttpUrlEncodingCodec.prototype.decodeKey = X_WWW_FORM_UrlEncodingCodec.prototype.decodeKey;
+    HttpUrlEncodingCodec.prototype.encodeKey = X_WWW_FORM_UrlEncodingCodec.prototype.encodeKey;
+    HttpUrlEncodingCodec.prototype.decodeValue = X_WWW_FORM_UrlEncodingCodec.prototype.decodeValue;
+    HttpUrlEncodingCodec.prototype.encodeValue = X_WWW_FORM_UrlEncodingCodec.prototype.encodeValue;
+  }
+
+  private static encode(v: string): string {
+    return encodeURIComponent(v).replace(/%20/gi, '+');
+  }
+
+  private static decode(v: string): string {
+    return decodeURIComponent(v.replace(/\\+/gi, '%20'));
+  }
+
+  encodeKey(key: string): string {
+    return X_WWW_FORM_UrlEncodingCodec.encode(key);
+  }
+  encodeValue(value: string): string {
+    return X_WWW_FORM_UrlEncodingCodec.encode(value);
+  }
+  decodeKey(key: string): string {
+    return X_WWW_FORM_UrlEncodingCodec.decode(key);
+  }
+  decodeValue(value: string): string {
+    return X_WWW_FORM_UrlEncodingCodec.decode(value);
+  }
+}
+
