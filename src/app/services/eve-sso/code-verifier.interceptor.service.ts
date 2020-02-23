fcd070421224b8f491756c3ff2116e3fbdbcc053
Nikolay Postnikov <nick.postnikov@gmail.com>
2020-02-23 13:45:38 +0300

    Fix eslint problems

--- src/app/services/eve-sso/code-verifier.interceptor.service.ts
+++ src/app/services/eve-sso/code-verifier.interceptor.service.ts
@@ -3,7 +3,8 @@ import {
     HttpRequest,
     HttpHandler,
     HttpEvent,
-    HttpInterceptor
+    HttpInterceptor,
+    HttpParams
   } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { OAuthService } from 'angular-oauth2-oidc';
@@ -18,14 +19,15 @@ export class CodeVerifierInterceptorService implements HttpInterceptor {
 
   constructor(private oauth: OAuthService) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const param_name = 'code_verifier';
-    if (request.url === this.oauth.tokenEndpoint && request.method === 'POST' && request.body.has(param_name))
-    {
-      const cv = b64urlEncode(request.body.get(param_name));
-      request = request.clone({
-        body: request.body.set(param_name, cv)
-      });
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (request.url === this.oauth.tokenEndpoint && request.method === 'POST' && typeof request.body === 'object') {
+      const param_name = 'code_verifier';
+      const params = request.body as HttpParams;
+      if (params.has(param_name))
+      {
+        const cv = b64urlEncode(params.get(param_name));
+        request = request.clone({ body: params.set(param_name, cv) });
+      }
     }
     return next.handle(request);
   }
