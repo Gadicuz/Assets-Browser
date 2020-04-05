b0ade32a068a3a9e98b7965a9f037eec42799780
Nikolay Postnikov <nick.postnikov@gmail.com>
2020-04-05 19:02:17 +0300

    Refactor 'undef' pipe

--- src/app/pipes/undef.pipe.ts
+++ src/app/pipes/undef.pipe.ts
@@ -3,6 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'undef' })
 export class UndefPipe implements PipeTransform {
   transform(value: unknown, undefValue: string): unknown {
-    return value == undefined ? undefValue : value;
+    return value ?? undefValue;
   }
 }
